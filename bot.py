62eff79d6cd8763a34b119c98fbaa419770e46c5
WenkG <81651465+WenkG@users.noreply.github.com>
2021-03-31 17:38:22 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -34,20 +34,20 @@ def at_converter(message):
         insta_link = "https://ancient-tor-24688.herokuapp.com/".format(at_text[1:])
         bot.reply_to(message, insta_link)
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://ancient-tor-24688.herokuapp.com/' + TOKEN)
-    return "!", 200
-
-
+# @server.route('/' + TOKEN, methods=['POST'])
+# def getMessage():
+#     json_string = request.get_data().decode('utf-8')
+#     update = telebot.types.Update.de_json(json_string)
+#     bot.process_new_updates([update])
+#     return "!", 200
+#
+#
+# @server.route("/")
+# def webhook():
+#     bot.remove_webhook()
+#     bot.set_webhook(url='https://ancient-tor-24688.herokuapp.com/' + TOKEN)
+#     return "!", 200
+#
+#
 # if __name__ == "__main__":
 #     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
